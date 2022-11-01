c829c5f94c76eb6e4e117f51033388241dffe312
Philippe Ombredanne <pombredanne@nexb.com>
2022-11-01 20:03:24 +0100

    Use simpler settings like in ScanCode.io
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- packagedb/manage.py
+++ packagedb/manage.py
@@ -12,7 +12,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "packagedbio.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "packagedbio.settings")
 
     from django.core.management import execute_from_command_line
 
