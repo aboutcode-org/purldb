69b25ef37f1877a089ee45735ce089dd4d85ae54
Jono Yang <jyang@nexb.com>
2023-04-19 18:21:45 -0700

    Create API endpoint #75
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- minecode/api.py
+++ minecode/api.py
@@ -7,11 +7,19 @@
 # See https://aboutcode.org for more information about nexB OSS projects.
 #
 
-
 from rest_framework import serializers
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from packageurl import PackageURL
 
+# UnusedImport here!
+# But importing the mappers and visitors module triggers routes registration
+from minecode import visitors  # NOQA
+from minecode import priority_router
 from minecode.models import ResourceURI
+from minecode.models import PriorityResourceURI
 
 
 class ResourceURISerializer(serializers.ModelSerializer):
@@ -23,3 +31,45 @@ class ResourceURIViewSet(viewsets.ModelViewSet):
     queryset = ResourceURI.objects.all()
     serializer_class = ResourceURISerializer
     paginate_by = 10
+
+
+class PriorityResourceURISerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PriorityResourceURI
+        fields = '__all__'
+
+
+class PriorityResourceURIViewSet(viewsets.ModelViewSet):
+    queryset = PriorityResourceURI.objects.all()
+    serializer_class = PriorityResourceURISerializer
+    paginate_by = 10
+
+    @action(detail=True, methods=["post"])
+    def add_to_queue(self, request, *args, **kwargs):
+        priority_resource_uri = self.get_object()
+        purl = request.data.get('purl')
+
+        # validate purl
+        try:
+            package_url = PackageURL.from_string(purl)
+        except ValueError as e:
+            message = {
+                'status': f'purl validation error: {e}'
+            }
+            return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+        # see if its routeable
+        if not priority_router.is_routable(purl):
+            message = {
+                'status': f'purl {purl} cannot be fetched'
+            }
+            return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+        # add to queue
+        PriorityResourceURI.objects.create(uri=purl, package_url=purl)
+
+        message = {
+            'status': f'purl {purl} added to queue'
+        }
+        return Response(message)
