172494cda3af3222fb5c426df2e10a5caac2d337
Jono Yang <jyang@nexb.com>
2023-05-16 15:53:31 -0700

    Use bulk_update in migration #95
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- packagedb/migrations/0065_set_package_content_field.py
+++ packagedb/migrations/0065_set_package_content_field.py
@@ -4,12 +4,33 @@ from django.db import migrations
 
 
 def set_package_content_field(apps, schema_editor):
+    # TODO: have separate job that updates the package_content field
     Package = apps.get_model("packagedb", "Package")
-    packages = Package.objects.filter(package_content__isnull=True)
-    for package in packages:
+    packages = Package.objects.filter(
+        package_content__isnull=True
+    ).iterator(
+        chunk_size=5000
+    )
+    updated = []
+    for i, package in enumerate(packages):
+        if not i % 5000:
+            Package.objects.bulk_update(
+                objs=updated,
+                fields=[
+                    "package_content"
+                ]
+            )
+            updated = []
         if 'source' in package.qualifiers:
             package.package_content = Package.PackageContentType.SOURCE
-            package.save()
+            updated.append(package)
+    if updated:
+        Package.objects.bulk_update(
+            objs=updated,
+            fields=[
+                "package_content"
+            ]
+        )
 
 
 class Migration(migrations.Migration):
