6443f18a59f4bdcfa37803f286a26284e590cd67
Jono Yang <jyang@nexb.com>
2023-09-29 10:55:32 -0700

    Bulk update Packages #179
        * Add logging messages
    
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- minecode/management/commands/get_maven_release_dates.py
+++ minecode/management/commands/get_maven_release_dates.py
@@ -8,16 +8,17 @@
 #
 
 from dateutil.parser import parse as dateutil_parse
+from os.path import dirname
 import logging
 import sys
 
 import requests
 
 from minecode.management.commands import VerboseCommand
-from packagedb.models import Package
 from minecode.visitors.maven import collect_links_from_text
 from minecode.visitors.maven import filter_for_artifacts
-from os.path import dirname
+from packagedb.models import Package
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout)
@@ -29,12 +30,18 @@ if TRACE:
 
 
 class Command(VerboseCommand):
-    help = ''
+    help = 'Get and set release_date for Maven Packages'
 
     def handle(self, *args, **options):
-        maven_packages = Package.objects.filter(type='maven', release_date=None)
-        for package in maven_packages:
+        queryset = Package.objects.filter(type='maven', release_date=None)
+        object_count = queryset.count()
+        chunk_size = 2000
+        iterator = queryset.iterator(chunk_size=chunk_size)
+        unsaved_objects = []
+        for index, package in enumerate(iterator, start=1):
             download_url = package.download_url
+            package_url = package.package_url
+            logger.info(f'Updating release_date for package {package_url} ({download_url})')
             package_version_page_url = dirname(download_url)
             filename = download_url.rsplit('/')[-1]
             response = requests.get(package_version_page_url)
@@ -42,8 +49,21 @@ class Command(VerboseCommand):
                 timestamps_by_links = collect_links_from_text(response.text, filter=filter_for_artifacts)
                 timestamp = timestamps_by_links.get(filename)
                 if not timestamp:
+                    logger.info(f'\tCould not get release_date for package {package_url} ({download_url})')
                     continue
                 timestamp = dateutil_parse(timestamp)
                 package.release_date = timestamp
-                # TODO: do batch update
-                package.save()
+                unsaved_objects.append(package)
+                logger.info(f'\t{package_url} ({download_url}) release_date has been updated to {timestamp}')
+            else:
+                logger.info(f'\t{package_url} not updated: error encountered when visiting {package_version_page_url}')
+            if not (index % chunk_size) and unsaved_objects:
+                logger.info(f'{index:,} / {object_count:,} Packages processed')
+
+        logger.info('Updating Package objects...')
+        updated_packages_count = Package.objects.bulk_update(
+            objs=unsaved_objects,
+            fields=['release_date'],
+            batch_size=1000,
+        )
+        logger.info(f'Updated {updated_packages_count} Package objects')
