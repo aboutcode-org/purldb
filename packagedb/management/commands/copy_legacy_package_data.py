6640a8e56b370fb9502351edac543b43946f6fb9
Jono Yang <jyang@nexb.com>
2023-10-17 18:45:32 +0000

    Create new instances when copying data
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- packagedb/management/commands/copy_legacy_package_data.py
+++ packagedb/management/commands/copy_legacy_package_data.py
@@ -29,6 +29,7 @@ class Command(VerboseCommand):
         unsaved_dependencies = []
         unsaved_parties = []
 
+        print(f"Copying {package_count:,} Packages from the 'minecode' database to the 'default' database")
         i = 0
         for package in iterator:
             if Package.objects.filter(download_url=package.download_url).exists():
@@ -48,11 +49,69 @@ class Command(VerboseCommand):
                 unsaved_parties = []
                 print(f"  {i:,} / {package_count:,} saved")
             else:
+                new_package = Package(
+                    filename=package.filename,
+                    package_content=package.package_content,
+                    type=package.type,
+                    namespace=package.namespace,
+                    name=package.name,
+                    version=package.version,
+                    qualifiers=package.qualifiers,
+                    subpath=package.subpath,
+                    primary_language=package.primary_language,
+                    description=package.description,
+                    release_date=package.release_date,
+                    keywords=package.keywords,
+                    homepage_url=package.homepage_url,
+                    download_url=package.download_url,
+                    bug_tracking_url=package.bug_tracking_url,
+                    code_view_url=package.code_view_url,
+                    vcs_url=package.vcs_url,
+                    repository_homepage_url=package.repository_homepage_url,
+                    repository_download_url=package.repository_download_url,
+                    api_data_url=package.api_data_url,
+                    size=package.size,
+                    md5=package.md5,
+                    sha1=package.sha1,
+                    sha256=package.sha256,
+                    sha512=package.sha512,
+                    copyright=package.copyright,
+                    holder=package.holder,
+                    declared_license_expression=package.declared_license_expression,
+                    license_detections=package.license_detections,
+                    other_license_expression=package.other_license_expression,
+                    other_license_detections=package.other_license_detections,
+                    extracted_license_statement=package.extracted_license_statement,
+                    source_packages=package.source_packages,
+                    extra_data=package.extra_data,
+                    datasource_id=package.datasource_id,
+                    file_references=package.file_references,
+                )
                 unsaved_packages.append(package)
-                dependencies = package.dependencies.all()
-                unsaved_dependencies.extend(list(dependencies))
-                parties = package.parties.all()
-                unsaved_parties.extend(list(parties))
+
+                for dependency in package.dependencies.all():
+                    new_dependency = DependentPackage(
+                        package=new_package,
+                        purl=dependency.purl,
+                        extracted_requirements=dependency.extracted_requirement,
+                        scope=dependency.scope,
+                        is_runtime=dependency.is_runtime,
+                        is_optional=dependency.is_optional,
+                        is_resolved=dependency.is_resolved,
+                    )
+                    unsaved_dependencies.append(new_dependency)
+
+                for party in package.parties.all():
+                    new_party = Party(
+                        package=new_package,
+                        type=party.type,
+                        role=party.role,
+                        name=party.name,
+                        email=party.email,
+                        url=party.url,
+                    )
+                    unsaved_parties.append(new_party)
+
                 i += 1
 
         if unsaved_packages:
