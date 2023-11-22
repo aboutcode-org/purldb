4659ccbc0871478edacfa83eacd5e02865c4f9fc
Jono Yang <jyang@nexb.com>
2023-11-22 13:49:39 -0800

    Create new django app matchcodeio #224
        * Add scancodeio under extra_requires
    
    Signed-off-by: Jono Yang <jyang@nexb.com>

---
+++ matchcodeio/settings.py
@@ -0,0 +1,35 @@
+#
+# Copyright (c) nexB Inc. and others. All rights reserved.
+# purldb is a trademark of nexB Inc.
+# SPDX-License-Identifier: Apache-2.0
+# See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
+# See https://github.com/nexB/purldb for support or download.
+# See https://aboutcode.org for more information about nexB OSS projects.
+#
+
+from scancodeio.settings import *
+
+
+INSTALLED_APPS += [
+    "packagedb",
+    "matchcode",
+]
+
+
+# Database
+
+DATABASES.update(
+    {
+        'packagedb': {
+            'ENGINE': env.str('PACKAGEDB_DB_ENGINE', 'django.db.backends.postgresql'),
+            'HOST': env.str('PACKAGEDB_DB_HOST', 'localhost'),
+            'NAME': env.str('PACKAGEDB_DB_NAME', 'packagedb'),
+            'USER': env.str('PACKAGEDB_DB_USER', 'packagedb'),
+            'PASSWORD': env.str('PACKAGEDB_DB_PASSWORD', 'packagedb'),
+            'PORT': env.str('PACKAGEDB_DB_PORT', '5432'),
+            'ATOMIC_REQUESTS': True,
+        }
+    }
+)
+
+DATABASE_ROUTERS = ["matchcodeio.dbrouter.PackageDBReadOnlyRouter",]
---
+++ matchcodeio/wsgi.py
@@ -0,0 +1,25 @@
+#
+# Copyright (c) nexB Inc. and others. All rights reserved.
+# purldb is a trademark of nexB Inc.
+# SPDX-License-Identifier: Apache-2.0
+# See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
+# See https://github.com/nexB/purldb for support or download.
+# See https://aboutcode.org for more information about nexB OSS projects.
+#
+
+"""
+WSGI config for MatchCode.io.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "matchcodeio.settings")
+
+application = get_wsgi_application()
