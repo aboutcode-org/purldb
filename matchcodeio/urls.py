f4811733f639c10f110100aed3a14f8a52b5d9c9
Jono Yang <jyang@nexb.com>
2023-12-04 11:35:55 -0800

    Only expose matching endpoint on matchcodeio #224
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- matchcodeio/urls.py
+++ matchcodeio/urls.py
@@ -12,25 +12,14 @@ from django.urls import path
 from django.views.generic import RedirectView
 from rest_framework import routers
 
-from clearcode.api import CDitemViewSet
-from packagedb.api import PackageViewSet
-from packagedb.api import PackageSetViewSet
-from packagedb.api import ResourceViewSet
 from matchcode.api import MatchingViewSet
-from minecode.api import PriorityResourceURIViewSet
-from scanpipe.api.views import RunViewSet
 
 
 api_router = routers.DefaultRouter()
-api_router.register('packages', PackageViewSet)
-api_router.register('package_sets', PackageSetViewSet)
-api_router.register('resources', ResourceViewSet)
 api_router.register('matching', MatchingViewSet)
-api_router.register('runs', RunViewSet)
-api_router.register('cditems', CDitemViewSet, 'cditems')
-api_router.register('on_demand_queue', PriorityResourceURIViewSet)
 
 urlpatterns = [
     path('api/', include(api_router.urls)),
+    path("", include("scanpipe.urls")),
     path('', RedirectView.as_view(url='api/')),
 ]
