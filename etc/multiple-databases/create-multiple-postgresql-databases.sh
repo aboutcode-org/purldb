b5a3c47421cc3b3524f0e2e21c631d01e5b92f61
Jono Yang <jyang@nexb.com>
2024-01-10 16:44:13 -0800

    Copy .env to /etc/scancodeio during testing #224
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- etc/multiple-databases/create-multiple-postgresql-databases.sh
+++ etc/multiple-databases/create-multiple-postgresql-databases.sh
@@ -4,21 +4,20 @@ set -e
 set -u
 
 function create_user_and_database() {
-    local dbinfo=$1
-    IFS=":" read -r database user password <<< "$dbinfo"
+    local database=$1
 
-    echo "Creating database '$database' with user '$user' and password '$password'"
+    echo "Creating database '$database' with user '$database' and password '$database'"
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-      CREATE USER $user;
-      ALTER USER $user WITH ENCRYPTED PASSWORD '$password';
+      CREATE USER $database;
+      ALTER USER $database WITH ENCRYPTED PASSWORD '$database';
       CREATE DATABASE $database;
-      GRANT ALL PRIVILEGES ON DATABASE $database TO $user;
+      GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
 EOSQL
 }
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
 	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
-	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
+	for db in $(echo $POSTGRES_MULTIPLE_DATABASES); do
 		create_user_and_database $db
 	done
 	echo "Multiple databases created"
