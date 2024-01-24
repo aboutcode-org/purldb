f706fdd32a03c6ea061da416bad3b68ba234385a
Tushar Goel <tushar.goel.dav@gmail.com>
2024-01-24 17:31:58 +0530

    Add support for remote
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>

--- clearcode/store_scans.py
+++ clearcode/store_scans.py
@@ -17,16 +17,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from collections import defaultdict
 from clearcode.models import CDitem
 from clearcode.cdutils import Coordinate
 from clearcode.cdutils import str2coord
 from django.db.models import Q
 from hashlib import sha512
 import json
+import requests
 from packageurl import PackageURL
 from pathlib import Path
 from git import Repo
-
+import os
 """
 The input is a bunch of scans from ClearlyDefined and 
 the output is a bunch of git repositories with commited and 
@@ -66,12 +68,15 @@ repo_names = [hex(hash)[2:].zfill(3) for hash in range(4096)]
 def store_scancode_scans_from_cd_items(work_dir, github_org="", count=0):
     """
     Iterate over CDItem objects with scancode scans.  
-    Save and commit them in git repositories in work dir
+    Save and commit them in git repositories in work dir.
+    Process a maximum of count items and process all items if
+    count is 0
     """
     cd_items = CDitem.objects.filter(~Q(content=b''), path__contains="tool/scancode")
-    x = get_cd_item_by_repo_name(cd_items=cd_items)
-    print(x)
-    for repo_name, cd_items in x.items():
+    if count:
+        cd_items = cd_items[:count] 
+    for purl_hash, cd_items in get_cd_item_by_purl_hash(cd_items=cd_items).items():
+        commit_count = 0
         for cd_item in cd_items:
             data = cd_item.data
             cd_url = data.get("_metadata", {}).get("url")
@@ -79,21 +84,24 @@ def store_scancode_scans_from_cd_items(work_dir, github_org="", count=0):
             if not is_valid_coordinate(coordinate):
                 #TODO: this is a bug, we need to log it
                 continue
-            purl = coordinate.to_purl()
-            # temporary hack iterate on all hashes until we store 
-            # hashes in CDItem
-            if repo_name!=get_repo_name(purl):
-                continue
-            # Initialize the repo - Get or Create the git repo
-            repo = get_or_init_repo(repo_name=repo_name, work_dir=work_dir, github_org=github_org)
             scancode_scan = data.get("content")
-            if scancode_scan:
-                add_scancode_scan(scancode_scan, purl, repo)
-                # TODO: push for every 10 commits
+            if not scancode_scan:
+                continue
+            repo = get_or_init_repo(repo_name=purl_hash, work_dir=work_dir, repo_namespace=github_org, pull=False)
+            purl = coordinate.to_purl()
+            if add_scancode_scan(scancode_scan=scancode_scan, purl=purl, repo=repo):
+                commit_count += 1
+            if commit_count % 10 == 0:
+                print(".", end="")
+                origin = repo.remote(name='origin')
+                origin.push()
 
 
-def get_cd_item_by_repo_name(cd_items):
-    cd_item_by_repo_name = {}
+def get_cd_item_by_purl_hash(cd_items):
+    """
+    Return a mapping of {purl_hash: [CDItem,....]} 
+    """
+    cd_item_by_purl_hash = defaultdict(list)
     for cd_item in cd_items:
         data = cd_item.data
         cd_url = data.get("_metadata", {}).get("url")
@@ -102,28 +110,29 @@ def get_cd_item_by_repo_name(cd_items):
             #TODO: this is a bug, we need to log it
             continue
         purl = coordinate.to_purl()
-        repo_name = get_repo_name(purl=purl)
-        if cd_item_by_repo_name.get(repo_name):
-            cd_item_by_repo_name[repo_name].append(cd_item)
-        else:
-            cd_item_by_repo_name[repo_name] = [cd_item]
-    return cd_item_by_repo_name
+        purl_hash = get_purl_hash(purl=purl)
+        cd_item_by_purl_hash[purl_hash].append(cd_item)
+    return cd_item_by_purl_hash
 
 
-def add_scancode_scan(scancode_scan, purl, repo):
+def add_scancode_scan(repo, purl, scancode_scan):
     """
     Save and commit scancode scan for purl to git repo.
+    Return true if we commited else false
     """
     purl_data_dir = get_or_create_dir_for_purl(purl=purl, repo=repo)
     scancode_scan_path = purl_data_dir / "scancode-toolkit-scan.json"
     with open(scancode_scan_path, "w") as f:
         json.dump(scancode_scan,f,indent=2)
-    repo.index.add([scancode_scan_path])
-    repo.index.commit(message=f"Add scancode-toolkit scan for {purl}")
+
+    if repo.is_dirty():
+        repo.index.add([scancode_scan_path])
+        repo.index.commit(message=f"Add scancode-toolkit scan for {purl}")
+        return True
 
 
 def is_valid_coordinate(coordinate):
-    return coordinate.type and coordinate.name
+    return coordinate.type and coordinate.name and coordinate.version
 
 
 def get_or_create_dir_for_purl(purl, repo):
@@ -142,7 +151,7 @@ def get_purl_path(purl):
     return purl_path / purl.name / purl.version
 
 
-def get_repo_name(purl: PackageURL, length: int=3) -> str:
+def get_purl_hash(purl: PackageURL, length: int=3) -> str:
     """
     Return a short lower cased hash of a purl.
     """
@@ -152,38 +161,76 @@ def get_repo_name(purl: PackageURL, length: int=3) -> str:
     return short_hash.lower()
 
 
-# def run(directory, repo_url):
-#     try:
-#         repo = Repo.clone_from(repo_url, directory)
-#     except:
-#         repo = Repo(directory)
-#         if repo.remotes.origin.fetch()[0].commit != repo.head.commit:
-#             # Pull the latest changes from the remote repository
-#             repo.remotes.origin.pull()
-
-#     store_scancode_scans_from_cd_items(work_dir = directory)
-#     RepoAdder.add_all(repo)
-#     RepoCommiter.commit(repo=repo, message="Push")
-
-
-def get_or_init_repo(repo_name: str, work_dir: Path, github_org: str= "", pull=False):
-    # TODO: Check if repo exists on remote
-    # TODO: If not exisiting on remote, create it on github_org
-    """If exists take a clone in the working directory, if repo already exists in working directory
-    then take the latest pull. Return the repo object"""
+def get_or_init_repo(repo_name: str, work_dir: Path, repo_namespace: str= "", pull=False):
+    """
+    Return a repo object for repo name and namespace 
+    and store it in the work dir. Clone if it does not 
+    exist optionally take the latest pull if it does exist.
+    """
+    # TODO: Manage org repo name
+    # MAYBE: CREATE ALL THE REPOS AT A TIME AND CLONE THEM LOCALLY
+    if repo_name not in get_github_repos(user_name="TG1999"):
+        repo_url = create_github_repo(repo_name=repo_name)
     repo_path = work_dir / repo_name
     if repo_path.exists():
-        return Repo(repo_path)
+        repo = Repo(repo_path)
+        if pull:
+            repo.origin.pull()
     else:
-        repo = Repo.init(repo_path)
-        return repo
+        repo = Repo.clone_from(repo_url, repo_path)
+    return repo
 
 
-def get_scan_download_url(purl:str, github_org:str, scan_file_name: str = "scancode-toolkit-scan.json"):
-    repo_name = get_repo_name(purl=purl)
+def get_scan_download_url(namespace:str, purl:str, scan_file_name: str = "scancode-toolkit-scan.json"):
+    purl_hash = get_purl_hash(purl=purl)
     purl_path = get_purl_path(purl)
-    return f"https://raw.githubusercontent.com/{github_org}/{repo_name}/main/{purl_path}/{scan_file_name}"
+    return f"https://raw.githubusercontent.com/{namespace}/{purl_hash}/main/{purl_path}/{scan_file_name}"
 
+
+def create_github_repo(repo_name, token=os.getenv("GH_TOKEN")):
+    headers = {
+        'Authorization': f'token {token}',
+        'Accept': 'application/vnd.github.v3+json'
+    }
+
+    data = {
+        'name': repo_name,
+    }
+
+    url = 'https://api.github.com/user/repos'
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 201:
+        print(f"Repository '{repo_name}' created successfully!")
+    else:
+        print(f"Failed to create repository. Status code: {response.status_code}")
+        print(response.text)
+
+
+def get_github_repos(user_name, token=os.getenv("GH_TOKEN")):
+    """
+    Yield full repo names for a user or org name, use the optional ``token`` if provided.
+    Full repo name is in the form user or org name / repo name
+    """
+    headers = {
+        'Accept': 'application/vnd.github.v3+json'
+    }
+    if token:
+        headers['Authorization'] = f'token {token}'
+
+    url = f'https://api.github.com/users/{user_name}/repos'
+    response = requests.get(url, headers=headers)
+
+    # TODO: We need have a way to handle failures from GH API
+    if not response.status_code == 200:
+        raise Exception(f"HTTP {response.status_code}: Failed to get repos for {user_name}")
+
+    data = response.json()
+    for repo_data in data:
+        full_repo_name = repo_data.get("full_name")
+        if full_repo_name:
+            yield full_repo_name
 
 if __name__ == "__main__":
     work_dir = Path("")
