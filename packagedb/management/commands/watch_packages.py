a4b1484e4729f58891bd55730fd00ca62494f658
Keshav Priyadarshi <git@keshav.space>
2024-02-02 22:38:33 +0530

    Remove duplicate code from watch_packages.py
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>

--- packagedb/management/commands/watch_packages.py
+++ packagedb/management/commands/watch_packages.py
@@ -10,11 +10,11 @@
 from commoncode import cliutils
 from django.core.management.base import BaseCommand
 from fetchcode.package_versions import SUPPORTED_ECOSYSTEMS
-from fetchcode.package_versions import versions
-from minecode.models import PriorityResourceURI
-from packagedb.models import Package
 from packageurl import PackageURL
 from univers.version_range import RANGE_CLASS_BY_SCHEMES
+
+from packagedb.models import Package
+from packagedb.tasks import get_and_index_new_purls
 
 VERSION_CLASS_BY_PACKAGE_TYPE = {
     pkg_type: range_class.version_class
@@ -25,7 +25,7 @@ PRIORITY_QUEUE_SUPPORTED_ECOSYSTEMS = ["maven", "npm"]
 
 
 class Command(BaseCommand):
-    help = "Watch the packages for their latest versions and add them to the priority queue for scanning and indexing."
+    help = "Watch all packages for their latest versions and add them to the priority queue for scanning and indexing."
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -33,7 +33,6 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        verbosity = options.get("verbosity")
         purl_value = options.get("purl")
 
         packages_qs = (
@@ -55,41 +54,10 @@ class Command(BaseCommand):
             show_percent=True,
         ) as packages:
             for package in packages:
-                version_class = VERSION_CLASS_BY_PACKAGE_TYPE.get(package.type)
-                latest_local = package.get_latest_version()
-                latest_local_version = version_class(latest_local.version)
-                all_versions = versions(str(package))
-                sorted_versions = sorted(
-                    [version_class(version.value) for version in all_versions]
-                )
+                error = get_and_index_new_purls(package.package_url)
 
-                if latest_local_version not in sorted_versions:
+                if error:
                     self.stdout.write(
-                        f"\nNOTICE: {latest_local} not found upstream.",
+                        error,
                         self.style.NOTICE,
                     )
-
-                new_versions = [v for v in sorted_versions if v > latest_local_version]
-                for version in new_versions:
-                    purl = str(
-                        PackageURL(
-                            type=package.type,
-                            namespace=package.namespace,
-                            name=package.name,
-                            version=str(version),
-                        )
-                    )
-
-                    priority_resource_uri = PriorityResourceURI.objects.insert(purl)
-
-                    if verbosity > 1:
-                        if priority_resource_uri:
-                            self.stdout.write(
-                                f"\nINFO: {purl} queued for indexing",
-                                self.style.SUCCESS,
-                            )
-                        else:
-                            self.stdout.write(
-                                f"\nINFO: {purl} already in queue for indexing",
-                                self.style.WARNING,
-                            )
