031533ca05ca89c0e77d2d19ae1cd145fd6fdc37
Tushar Goel <tushar.goel.dav@gmail.com>
2024-03-27 22:49:29 +0530

    Fix tests
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>

--- minecode/tests/test_indexing.py
+++ minecode/tests/test_indexing.py
@@ -140,4 +140,4 @@ class IndexingTest(MiningTestCase, JsonBasedTesting):
         result = Resource.objects.filter(package=self.package2)
         self.assertEqual(1, result.count())
 
-        self.assertEqual(result.extra_data, scan_data["files"][0]["extra_data"])
+        self.assertEqual(result.first().extra_data, scan_data["files"][0]["extra_data"])
