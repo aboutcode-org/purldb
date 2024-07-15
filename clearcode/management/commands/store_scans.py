a7463561bde94409c6faa0c150af31f744a79753
Tushar Goel <tushar.goel.dav@gmail.com>
2024-07-15 21:51:14 +0530

    Address review comments
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>

--- clearcode/management/commands/store_scans.py
+++ clearcode/management/commands/store_scans.py
@@ -1,7 +1,16 @@
+#
+# Copyright (c) nexB Inc. and others. All rights reserved.
+# purldb is a trademark of nexB Inc.
+# SPDX-License-Identifier: Apache-2.0
+# See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
+# See https://github.com/nexB/purldb for support or download.
+# See https://aboutcode.org for more information about nexB OSS projects.
+#
+
 from clearcode.store_scans import store_scancode_scans_from_cd_items
+from minecode.management.commands import VerboseCommand
 
-
-class Command(BaseCommand):
+class Command(VerboseCommand):
     help = 'Store scancode scans in git repositories'
 
     def add_arguments(self, parser):
