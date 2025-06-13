935e3fd5bc077ac026aabe0357481cf53423aecb
Jono Yang <jyang@nexb.com>
2025-06-12 17:17:33 -0700

    Update migration names #620
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- packagedb/migrations/0094_package_packagedb_p_package_d39839_idx.py
+++ packagedb/migrations/0094_package_packagedb_p_package_d39839_idx.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("packagedb", "0092_alter_party_email_alter_party_name"),
+        ("packagedb", "0093_update_pypi_package_content"),
     ]
 
     operations = [
