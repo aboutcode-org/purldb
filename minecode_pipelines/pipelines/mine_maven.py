a225502601b7eec9cf4bbcaca9886c656e60a856
Jono Yang <jyang@nexb.com>
2025-09-19 14:29:31 -0700

    Remove repo clone step #660
        * This is done in the collect_packages_from_maven step
    
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- minecode_pipelines/pipelines/mine_maven.py
+++ minecode_pipelines/pipelines/mine_maven.py
@@ -42,16 +42,12 @@ class MineMaven(PublishToFederatedCode):
     def steps(cls):
         return (
             cls.check_federatedcode_eligibility,
-            cls.clone_repos,
             cls.collect_packages_from_maven,
             cls.delete_cloned_repos,
         )
 
-    def clone_repos(self):
-        self.repos = maven.clone_repos(self.project, self.log)
-
     def collect_packages_from_maven(self):
-        maven.collect_packages_from_maven(self.project, self.log)
+        self.repos = maven.collect_packages_from_maven(self.project, self.log)
 
     def delete_cloned_repos(self):
         pipes.delete_cloned_repos(repos=self.repos, logger=self.log)
