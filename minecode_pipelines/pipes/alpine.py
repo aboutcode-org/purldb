8ad9c6a2357f7283c64798a4bf0b9579d1c86adf
Jono Yang <jyang@nexb.com>
2025-09-22 18:02:04 -0700

    Update AlpineCollector #665
        * Modify functions to return an Alpine PackageData from a parsed pkginfo string
    
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- minecode_pipelines/pipes/alpine.py
+++ minecode_pipelines/pipes/alpine.py
@@ -20,11 +20,12 @@
 # ScanCode.io is a free software code scanning tool from nexB Inc. and others.
 # Visit https://github.com/aboutcode-org/scancode.io for support and download.
 
-import os
+from shutil import rmtree
 from pathlib import Path
 
 import base64
-from packagedcode import models as scan_models
+from packageurl import PackageURL
+from packagedcode.models import PackageData, Party
 from scanpipe.pipes import federatedcode
 from scanpipe.pipes.fetch import fetch_http
 from scanpipe.pipes.scancode import extract_archives
@@ -40,43 +41,82 @@ MINECODE_DATA_ALPINE_REPO = "https://github.com/aboutcode-data/minecode-data-alp
 
 # Number of packages
 PACKAGE_BATCH_SIZE = 500
+ALPINE_LINUX_DISTROS = [
+    "edge",
+    "latest-stable",
+    "v3.0",
+    "v3.1",
+    "v3.10",
+    "v3.11",
+    "v3.12",
+    "v3.13",
+    "v3.14",
+    "v3.15",
+    "v3.16",
+    "v3.17",
+    "v3.18",
+    "v3.19",
+    "v3.2",
+    "v3.20",
+    "v3.21",
+    "v3.22",
+    "v3.3",
+    "v3.4",
+    "v3.5",
+    "v3.6",
+    "v3.7",
+    "v3.8",
+    "v3.9",
+]
+ALPINE_LINUX_ARCHS = [
+    "aarch64",
+    "armhf",
+    "armv7",
+    "ppc64le",
+    "s390x",
+    "x86",
+    "x86_64",
+]
 
 
-def build_packages(extracted_location, apk_download_url, purl=None):
-    """
-    Yield ScannedPackage built from Alpine Linux ( APK ) a `metadata` content
-    """
-
-    apk_index_file = Path(extracted_location) / "APKINDEX"
-
-    with open(apk_index_file, encoding="utf-8") as f:
-        parsed_pkginfo = parse_apkindex(f.read())
-
-    extracted_pkginfo = get_package_by_name(parsed_pkginfo, purl.name)
-    if not extracted_pkginfo:
-        return
-
-    description = extracted_pkginfo.get("description")
+def build_package(extracted_pkginfo, distro=None):
+    name = extracted_pkginfo.get("name")
     version = extracted_pkginfo.get("version")
-    extracted_license_statement = extracted_pkginfo.get("license")
+    arch = extracted_pkginfo.get("arch")
+    qualifiers = {
+        "arch": arch,
+    }
+    if distro:
+        qualifiers.update({"distro": distro})
 
     parties = []
     maintainers = extracted_pkginfo.get("maintainer")
     if maintainers:
         name, email = debutils.parse_email(maintainers)
         if name:
-            party = scan_models.Party(name=name, role="maintainer", email=email)
+            party = Party(name=name, role="maintainer", email=email)
             parties.append(party)
 
+    description = extracted_pkginfo.get("description")
+    extracted_license_statement = extracted_pkginfo.get("license")
     repository_homepage_url = extracted_pkginfo.get("url")
     size = extracted_pkginfo.get("size")
     apk_checksum = extracted_pkginfo.get("checksum")
     sha1 = apk_checksum_to_sha1(apk_checksum)
+    apk_download_url = f"https://dl-cdn.alpinelinux.org/alpine/{distro}/main/{arch}/{name}-{version}.apk"
 
-    download_data = dict(
+    purl = PackageURL(
         type="apk",
-        name=purl.name,
+        namespace="alpine",
+        name=name,
         version=version,
+        qualifiers=qualifiers
+    )
+    download_data = dict(
+        type=purl.type,
+        namespace=purl.namespace,
+        name=purl.name,
+        version=purl.version,
         qualifiers=purl.qualifiers,
         description=description,
         repository_homepage_url=repository_homepage_url,
@@ -85,12 +125,11 @@ def build_packages(extracted_location, apk_download_url, purl=None):
         size=size,
         sha1=sha1,
         download_url=apk_download_url,
+        datasource_id = "alpine_metadata",
     )
+    package = PackageData.from_data(download_data)
 
-    package = scan_models.PackageData.from_data(download_data)
-    package.datasource_id = "alpine_metadata"
-    package.set_purl(purl)
-    yield package
+    return package
 
 
 def parse_apkindex(data: str):
@@ -98,14 +137,13 @@ def parse_apkindex(data: str):
     Parse an APKINDEX format string into a list of package dictionaries.
     https://wiki.alpinelinux.org/wiki/Apk_spec
     """
-    packages = []
     current_pkg = {}
 
     for line in data.splitlines():
         line = line.strip()
         if not line:
             if current_pkg:
-                packages.append(current_pkg)
+                yield current_pkg
                 current_pkg = {}
             continue
 
@@ -147,13 +185,7 @@ def parse_apkindex(data: str):
             current_pkg[field] = value
 
     if current_pkg:
-        packages.append(current_pkg)
-
-    return packages
-
-
-def get_package_by_name(packages, name):
-    return next((pkg for pkg in packages if pkg["name"] == name), None)
+        yield current_pkg
 
 
 def apk_checksum_to_sha1(apk_checksum: str) -> str:
@@ -181,24 +213,33 @@ class AlpineCollector:
 
     def __init__(self, index_location=None):
         if index_location:
+            self.index_download = None
             self.index_location = index_location
         else:
-            download_location = self._fetch_index()
-            extract_archives(location=download_location)
-            self.index_location = f"{download_location}-extract/"
-        self.index_location_given = bool(index_location)
+            self.index_download = self._fetch_index()
+            extract_archives(location=self.index_download.path)
+            self.index_location = f"{self.index_download.path}-extract/APKINDEX"
 
     def __del__(self):
-        if self.index_location and not self.index_location_given:
-            os.remove(self.index_location)
+        if self.index_download:
+            rmtree(self.index_download.directory)
 
     def _fetch_index(self, uri=ALPINE_APKINDEX_URL):
         """
         Return a temporary location where the alpine index was saved.
         """
         index = fetch_http(uri)
-        return index.path
+        return index
 
     def get_packages(self, logger=None):
         """Yield Package objects from alpine index"""
-        pass
+
+        with open(self.index_location, encoding="utf-8") as f:
+            for pkg in parse_apkindex(f.read()):
+                pd = build_package(pkg, distro="latest-stable")
+                current_purl = PackageURL(
+                    type=pd.type,
+                    namespace=pd.namespace,
+                    name=pd.name,
+                )
+                yield current_purl, pd
