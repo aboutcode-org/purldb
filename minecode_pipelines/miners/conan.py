dfcba65c3795fed23c5c4d1b5c123f9fc791aa61
ziad hany <ziadhany2016@gmail.com>
2025-09-25 11:25:02 +0300

    Fix conan test
    Signed-off-by: ziad hany <ziadhany2016@gmail.com>

--- minecode_pipelines/miners/conan.py
+++ minecode_pipelines/miners/conan.py
@@ -9,10 +9,11 @@
 
 from pathlib import Path
 import saneyaml
+
 from scanpipe.pipes.federatedcode import commit_changes
 from scanpipe.pipes.federatedcode import push_changes
 from minecode_pipelines import VERSION
-from minecode_pipelines.pipes.conan import store_canon_packages
+from minecode_pipelines.pipes.conan import store_conan_packages
 
 PACKAGE_BATCH_SIZE = 1000
 
@@ -41,7 +42,7 @@ def mine_and_publish_conan_packageurls(conan_index_repo, cloned_data_repo, logge
         file_counter += 1
         push_commit = file_counter >= PACKAGE_BATCH_SIZE or idx == len(yml_files)
 
-        result_store = store_canon_packages(package, versions, cloned_data_repo)
+        result_store = store_conan_packages(package, versions, cloned_data_repo)
         if result_store:
             purl_file, base_purl = result_store
             logger(f"writing packageURLs for package: {base_purl} at: {purl_file}")
