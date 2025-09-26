e6de88d5fc27d3f19639c919762782997a83801b
Keshav Priyadarshi <git@keshav.space>
2025-09-26 17:08:04 +0530

    Include batch count in commit message
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>

--- minecode_pipelines/pipelines/mine_nuget.py
+++ minecode_pipelines/pipelines/mine_nuget.py
@@ -20,30 +20,27 @@
 # ScanCode.io is a free software code scanning tool from nexB Inc. and others.
 # Visit https://github.com/aboutcode-org/scancode.io for support and download.
 
-import json
+
 from pathlib import Path
 
 from minecode_pipelines.pipes import nuget
-from minecode_pipelines.pipes import write_packageurls_to_file
-from packageurl import PackageURL
 
-from aboutcode.hashid import get_package_base_dir
-from aboutcode.pipeline import LoopProgress
 from scanpipe.pipelines import Pipeline
 from scanpipe.pipes import federatedcode
 
 
 class MineAndPublishNuGetPURLs(Pipeline):
     """
-    Mine all packageURLs from NuGet catalog and publish them to
-    a FederatedCode repo.
+    Mine and Publish NuGet PackageURLs.
+
+    Mine PackageURLs from AboutCode NuGet catalog mirror and publish
+    them to FederatedCode Git repository.
     """
 
     download_inputs = False
     CATALOG_REPO_URL = (
         "https://github.com/aboutcode-org/aboutcode-mirror-nuget-catalog.git"
     )
-    NUGET_PURL_METADATA_REPO = "https://github.com/aboutcode-data/minecode-data-nuget-test"
 
     @classmethod
     def steps(cls):
@@ -52,7 +49,7 @@ class MineAndPublishNuGetPURLs(Pipeline):
             cls.fetch_nuget_catalog,
             cls.mine_nuget_package_versions,
             cls.mine_and_publish_nuget_packageurls,
-            cls.clean_downloads,
+            cls.delete_cloned_repos,
         )
 
     def check_federatedcode_eligibility(self):
@@ -63,6 +60,7 @@ class MineAndPublishNuGetPURLs(Pipeline):
         federatedcode.check_federatedcode_configured_and_available()
 
     def fetch_nuget_catalog(self):
+        """Fetch NuGet package catalog from AboutCode mirror."""
         self.catalog_repo = federatedcode.clone_repository(
             repo_url=self.CATALOG_REPO_URL,
             logger=self.log,
@@ -70,77 +68,22 @@ class MineAndPublishNuGetPURLs(Pipeline):
 
     def mine_nuget_package_versions(self):
         """Mine NuGet package and versions from NuGet catalog."""
-        catalog = Path(self.catalog_repo.working_dir) / "catalog"
-        catalog_count = nuget.get_catalog_page_count(catalog / "index.json")
-        catalog_pages = catalog / "pages"
-
-        self.package_versions = {}
-        self.skipped_packages = set()
-        self.log(f"Collecting versions from {catalog_count:,d} NuGet catalog.")
-        progress = LoopProgress(total_iterations=catalog_count, logger=self.log)
-        for page in progress.iter(catalog_pages.rglob("*.json")):
-            with page.open("r", encoding="utf-8") as f:
-                page_catalog = json.load(f)
-
-            nuget.collect_package_versions(
-                events=page_catalog["items"],
-                package_versions=self.package_versions,
-                skipped_packages=self.skipped_packages,
+        self.package_versions, self.skipped_packages = (
+            nuget.mine_nuget_package_versions(
+                catalog_path=Path(self.catalog_repo.working_dir),
+                logger=self.log,
             )
-        self.log(
-            f"Collected versions for {len(self.package_versions):,d} NuGet package."
         )
 
     def mine_and_publish_nuget_packageurls(self):
-        cloned_repo = federatedcode.clone_repository(
-            repo_url=self.NUGET_PURL_METADATA_REPO,
+        """Mine and publish PackageURLs from NuGet package versions."""
+        nuget.mine_and_publish_nuget_packageurls(
+            package_versions=self.package_versions,
             logger=self.log,
         )
-        file_to_commit = []
-        batch_size = 4000
-        file_processed = 0
-        nuget_package_count = len(self.package_versions)
-        progress = LoopProgress(
-            total_iterations=nuget_package_count,
-            logger=self.log,
-            progress_step=1,
-        )
 
-        self.log(f"Mine packageURL for {nuget_package_count:,d} NuGet packages.")
-        for base, versions in progress.iter(self.package_versions.items()):
-            package_base_dir = get_package_base_dir(purl=base)
-            purl_dict = PackageURL.from_string(base).to_dict()
-            del purl_dict["version"]
-            packageurls = [
-                PackageURL(**purl_dict, version=v).to_string() for v in versions
-            ]
-            purl_file = write_packageurls_to_file(
-                repo=cloned_repo,
-                base_dir=package_base_dir,
-                packageurls=sorted(packageurls),
-            )
-            file_to_commit.append(purl_file)
-            file_processed += 1
-
-            if len(file_to_commit) > batch_size:
-                federatedcode.commit_and_push_changes(
-                    commit_message=nuget.commit_message(),
-                    repo=cloned_repo,
-                    files_to_commit=file_to_commit,
-                    logger=self.log,
-                )
-                file_to_commit.clear()
-
-        self.log(f"Processed packageURL for {file_processed:,d} NuGet packages.")
-        federatedcode.commit_and_push_changes(
-            commit_message=nuget.commit_message(),
-            repo=cloned_repo,
-            files_to_commit=file_to_commit,
-            logger=self.log,
-        )
-        federatedcode.delete_local_clone(repo=cloned_repo)
-
-    def clean_downloads(self):
+    def delete_cloned_repos(self):
+        """Remove cloned catalog repository."""
         if self.catalog_repo:
             self.log("Removing cloned repository")
             federatedcode.delete_local_clone(repo=self.catalog_repo)
--- minecode_pipelines/pipes/nuget.py
+++ minecode_pipelines/pipes/nuget.py
@@ -26,9 +26,14 @@ import re
 
 from django.conf import settings
 
+from minecode_pipelines.pipes import write_packageurls_to_file
 from packageurl import PackageURL
 
+from aboutcode.hashid import get_package_base_dir
+from aboutcode.pipeline import LoopProgress
 from scancodeio import VERSION
+
+NUGET_PURL_METADATA_REPO = "https://github.com/aboutcode-data/minecode-data-nuget-test"
 
 
 def get_catalog_page_count(catalog_index):
@@ -40,11 +45,14 @@ def get_catalog_page_count(catalog_index):
 
 
 def collect_package_versions(events, package_versions, skipped_packages):
+    """Collect package versions from events in the NuGet package catalog."""
     for event in events or []:
         if event["@type"] != "nuget:PackageDetails":
             continue
         pkg_name = event["nuget:id"]
-        if bool(re.fullmatch(r"[a-z0-9]{46}", pkg_name)):
+
+        # Skip package names that resemble NuGet API key and can't be pushed to GitHub.
+        if bool(re.fullmatch(r"oy2[a-z0-9]{43}", pkg_name)):
             skipped_packages.add(pkg_name)
             continue
 
@@ -55,16 +63,102 @@ def collect_package_versions(events, package_versions, skipped_packages):
         package_versions[purl].add(event["nuget:version"])
 
 
-def commit_message():
+def mine_nuget_package_versions(catalog_path, logger):
+    """Mine NuGet package and versions from NuGet catalog."""
+    catalog = catalog_path / "catalog"
+    catalog_count = get_catalog_page_count(catalog / "index.json")
+    catalog_pages = catalog / "pages"
+
+    package_versions = {}
+    skipped_packages = set()
+    logger(f"Collecting versions from {catalog_count:,d} NuGet catalog.")
+    progress = LoopProgress(total_iterations=catalog_count, logger=logger)
+    for page in progress.iter(catalog_pages.rglob("*.json")):
+        with page.open("r", encoding="utf-8") as f:
+            page_catalog = json.load(f)
+
+        collect_package_versions(
+            events=page_catalog["items"],
+            package_versions=package_versions,
+            skipped_packages=skipped_packages,
+        )
+    logger(f"Collected versions for {len(package_versions):,d} NuGet package.")
+    return package_versions, skipped_packages
+
+
+def commit_message(commit_batch, total_commit_batch="many"):
     author_name = settings.FEDERATEDCODE_GIT_SERVICE_NAME
     author_email = settings.FEDERATEDCODE_GIT_SERVICE_EMAIL
     tool_name = "pkg:github/aboutcode-org/scancode.io"
 
     return f"""\
-        Collect PackageURLs from NuGet catalog
+        Collect PackageURLs from NuGet catalog ({commit_batch}/{total_commit_batch})
 
         Tool: {tool_name}@v{VERSION}
         Reference: https://{settings.ALLOWED_HOSTS[0]}
 
         Signed-off-by: {author_name} <{author_email}>
         """
+
+
+def get_nuget_purls_from_versions(base_purl, versions):
+    """Return PURLs for a NuGet `base_purls` from set of `versions`."""
+    purl_dict = PackageURL.from_string(base_purl).to_dict()
+    del purl_dict["version"]
+    return [PackageURL(**purl_dict, version=v).to_string() for v in versions]
+
+
+def mine_and_publish_nuget_packageurls(package_versions, logger):
+    """Mine and publish PackageURLs from NuGet package versions."""
+    from scanpipe.pipes import federatedcode
+
+    cloned_repo = federatedcode.clone_repository(
+        repo_url=NUGET_PURL_METADATA_REPO,
+        logger=logger,
+    )
+    file_to_commit = []
+    batch_size = 4000
+    file_processed = 0
+    commit_count = 1
+    nuget_package_count = len(package_versions)
+    progress = LoopProgress(
+        total_iterations=nuget_package_count,
+        logger=logger,
+        progress_step=1,
+    )
+
+    logger(f"Mine packageURL for {nuget_package_count:,d} NuGet packages.")
+    for base, versions in progress.iter(package_versions.items()):
+        package_base_dir = get_package_base_dir(purl=base)
+        packageurls = get_nuget_purls_from_versions(base_purl=base, versions=versions)
+
+        purl_file = write_packageurls_to_file(
+            repo=cloned_repo,
+            base_dir=package_base_dir,
+            packageurls=sorted(packageurls),
+        )
+        file_to_commit.append(purl_file)
+        file_processed += 1
+
+        if len(file_to_commit) > batch_size:
+            if federatedcode.commit_and_push_changes(
+                commit_message=commit_message(commit_count),
+                repo=cloned_repo,
+                files_to_commit=file_to_commit,
+                logger=logger,
+            ):
+                commit_count += 1
+            file_to_commit.clear()
+
+    federatedcode.commit_and_push_changes(
+        commit_message=commit_message(
+            commit_batch=commit_count,
+            total_commit_batch=commit_count,
+        ),
+        repo=cloned_repo,
+        files_to_commit=file_to_commit,
+        logger=logger,
+    )
+    logger(f"Processed PackageURL for {file_processed:,d} NuGet packages.")
+    logger(f"Pushed new PackageURL in {commit_count:,d} commits.")
+    federatedcode.delete_local_clone(repo=cloned_repo)
