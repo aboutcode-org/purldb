536250b89cbbf5998d386eac133e942ad111362b
Jono Yang <JonoYang@users.noreply.github.com>
2025-10-01 17:26:47 -0700

    Update alpine pipeline (#737)
    * Update ALPINE_LINUX_REPOS #665
    
    Signed-off-by: Jono Yang <jyang@nexb.com>
    
    * Bump version
    
    Signed-off-by: Jono Yang <jyang@nexb.com>
    
    ---------
    
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- minecode_pipelines/__init__.py
+++ minecode_pipelines/__init__.py
@@ -8,4 +8,4 @@
 #
 
 
-VERSION = "0.0.1b18"
+VERSION = "0.0.1b19"
--- minecode_pipelines/pipes/alpine.py
+++ minecode_pipelines/pipes/alpine.py
@@ -71,7 +71,6 @@ ALPINE_LINUX_DISTROS = [
 ALPINE_LINUX_REPOS = [
     "community",
     "main",
-    "releases",
     "testing",
 ]
 ALPINE_LINUX_ARCHS = [
