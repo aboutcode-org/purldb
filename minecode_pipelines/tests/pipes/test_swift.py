611a4dabe01852d7b40b07dfcd8aa1733cf7562d
Tushar Goel <34160672+TG1999@users.noreply.github.com>
2025-12-08 21:19:26 +0530

    Refactor swift mining pipeline for git deployment (#789)
    * Refactor swift
    
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>
    
    * Refactor swift
    
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>
    
    * Refactor swift
    
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>
    
    * Refactor swift
    
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>
    
    * Refactor tests for swift
    
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>
    
    * Refactor tests for swift
    
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>
    
    * Change checkpoint frequency
    
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>
    
    * Remove checkpoint from swift mining
    
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>
    
    * Bump version
    
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>
    
    ---------
    
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>

--- minecode_pipelines/tests/pipes/test_swift.py
+++ minecode_pipelines/tests/pipes/test_swift.py
@@ -7,50 +7,40 @@
 # See https://aboutcode.org for more information about nexB OSS projects.
 #
 
-import tempfile
 from pathlib import Path
 from unittest import TestCase
-from unittest.mock import Mock, patch
 import saneyaml
 from minecode_pipelines.pipes.swift import (
-    store_swift_packages,
     get_tags_and_commits_from_git_output,
+    generate_package_urls,
 )
 
 DATA_DIR = Path(__file__).parent.parent / "data" / "swift"
 
 
+def logger(msg):
+    print(msg)
+
+
 class SwiftPipelineTests(TestCase):
-    def _run_package_test(
-        self, mock_write, package_repo_url, commits_tags_file, expected_file, expected_path_parts
-    ):
-        # Load test input and expected output
+    def _run_package_test(self, package_repo_url, commits_tags_file, expected_file):
         with open(commits_tags_file, encoding="utf-8") as f:
             git_ls_remote = f.read()
+
         with open(expected_file, encoding="utf-8") as f:
             expected = saneyaml.load(f)
 
-        # Create a temporary working directory for the repo
-        with tempfile.TemporaryDirectory() as tmpdir:
-            repo = Mock()
-            repo.working_dir = tmpdir
+        tags_and_commits = get_tags_and_commits_from_git_output(git_ls_remote)
 
-            # Execute function under test
-            tags_and_commits = get_tags_and_commits_from_git_output(git_ls_remote)
-            store_swift_packages(package_repo_url, tags_and_commits, repo)
+        base_purl, generated_purls = generate_package_urls(
+            package_repo_url=package_repo_url,
+            tags_and_commits=tags_and_commits,
+            logger=logger,
+        )
 
-            # Verify function call
-            mock_write.assert_called_once()
-            _, kwargs = mock_write.call_args
-            base_purl, written_packages = kwargs["path"], kwargs["data"]
+        assert base_purl.to_string() == expected["base_purl"]
+        assert sorted(generated_purls) == sorted(expected["purls"])
 
-            # Expected file path
-            expected_base_purl = Path(tmpdir).joinpath(*expected_path_parts)
-
-            self.assertEqual(str(base_purl), str(expected_base_purl))
-            self.assertEqual(written_packages, expected)
-
-    @patch("minecode_pipelines.pipes.swift.write_data_to_yaml_file")
     def test_swift_safe_collection_access(self, mock_write):
         self._run_package_test(
             mock_write,
@@ -67,10 +57,8 @@ class SwiftPipelineTests(TestCase):
             ],
         )
 
-    @patch("minecode_pipelines.pipes.swift.write_data_to_yaml_file")
-    def test_human_string(self, mock_write):
+    def test_human_string(self):
         self._run_package_test(
-            mock_write,
             package_repo_url="https://github.com/zonble/HumanString.git",
             commits_tags_file=DATA_DIR / "commits_tags2.txt",
             expected_file=DATA_DIR / "expected2.yaml",
@@ -84,10 +72,8 @@ class SwiftPipelineTests(TestCase):
             ],
         )
 
-    @patch("minecode_pipelines.pipes.swift.write_data_to_yaml_file")
-    def test_swift_financial(self, mock_write):
+    def test_swift_financial(self):
         self._run_package_test(
-            mock_write,
             package_repo_url="https://github.com/zrluety/SwiftFinancial.git",
             commits_tags_file=DATA_DIR / "commits_tags3.txt",
             expected_file=DATA_DIR / "expected3.yaml",
@@ -101,10 +87,8 @@ class SwiftPipelineTests(TestCase):
             ],
         )
 
-    @patch("minecode_pipelines.pipes.swift.write_data_to_yaml_file")
-    def test_swift_xcf_sodium(self, mock_write):
+    def test_swift_xcf_sodium(self):
         self._run_package_test(
-            mock_write,
             package_repo_url="https://github.com/0xacdc/XCFSodium",
             commits_tags_file=DATA_DIR / "commits_tags4.txt",
             expected_file=DATA_DIR / "expected4.yaml",
