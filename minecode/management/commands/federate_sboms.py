639d0aa51e97da6b348a5c110bd486a25a51bcd2
Jono Yang <jyang@nexb.com>
2026-05-29 12:11:38 -0700

    Create command to federate Packages as SBOMs
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- minecode/management/commands/federate_sboms.py
+++ minecode/management/commands/federate_sboms.py
@@ -17,6 +17,7 @@ from minecode.management import federatedcode
 from minecode.management.commands import VerboseCommand
 from minecode_pipelines import pipes
 from packagedb import models as packagedb_models
+from packagedb import sbom
 
 
 logger = logging.getLogger(__name__)
@@ -39,7 +40,7 @@ def commit_message(commit_batch, total_commit_batch="many"):
     tool_name = "pkg:github/aboutcode-org/purldb"
 
     return f"""\
-        Save PackageURLs from PurlDB ({commit_batch}/{total_commit_batch})
+        Save CycloneDX SBOMs from PurlDB ({commit_batch}/{total_commit_batch})
 
         Tool: {tool_name}@v{settings.PURLDB_VERSION}
         Reference: https://{settings.ALLOWED_HOSTS[0]}
@@ -49,7 +50,7 @@ def commit_message(commit_batch, total_commit_batch="many"):
 
 
 class Command(VerboseCommand):
-    help = "Save and commit purls from PackageDB to FederatedCode repos."
+    help = "Save and commit CycloneDX SBOMs, generated from PackageDB package data. to FederatedCode repos."
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -73,7 +74,7 @@ class Command(VerboseCommand):
             name="aboutcode-data",
             remote_root_url="https://github.com/aboutcode-data",
         )
-        data_cluster = data_federation.get_cluster("purls")
+        data_cluster = data_federation.get_cluster("cyclonedx16_sboms")
 
         # TODO: do something more efficient
         files_to_commit = []
@@ -89,14 +90,14 @@ class Command(VerboseCommand):
                 working_path=working_path,
                 logger=logger.log,
             )
-            purl_file = pipes.write_packageurls_to_file(
+            package_sbom_data = sbom.to_cyclonedx(package)
+            sbom_file = pipes.write_package_data_to_file(
                 repo=package_repo,
-                relative_datafile_path=datafile_path,
-                packageurls=[package.purl],
-                append=True,
+                relative_api_package_metadata_datafile_path=datafile_path,
+                package_data=package_sbom_data,
             )
-            if purl_file not in files_to_commit:
-                files_to_commit.append(purl_file)
+            if sbom_file not in files_to_commit:
+                files_to_commit.append(sbom_file)
 
             if len(files_to_commit) == PACKAGE_BATCH_SIZE:
                 federatedcode.commit_and_push_changes(
@@ -105,7 +106,7 @@ class Command(VerboseCommand):
                     files_to_commit=files_to_commit,
                     logger=logger.log,
                 )
-                logger.log(f"Committed {i} purls to {package_repo_name}")
+                logger.log(f"Committed {i} SBOMs to {package_repo_name}")
                 files_to_commit.clear()
                 commit_batch += 1
 
@@ -116,6 +117,6 @@ class Command(VerboseCommand):
                 files_to_commit=files_to_commit,
                 logger=logger.log,
             )
-            logger.log(f"Committed {i} purls to {package_repo_name}")
+            logger.log(f"Committed {i} SBOMs to {package_repo_name}")
             files_to_commit.clear()
             commit_batch += 1
